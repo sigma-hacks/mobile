8cd1b32d3806493f813393f340f393788cb01882
Mariya Pelageicheva <pelagei4eva@gmail.com>
2023-09-15 16:40:09 +0500

    init

---
+++ android/app/src/main/kotlin/com/example/ekzh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ekzh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'login_screen/login_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
