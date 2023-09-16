0055ebf68fa79519dabf67e317007bebf95caa1b
Artsiom <artsiom.sazonau@kaseya.com>
2023-09-16 12:03:49 +0200

    removed test data

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,7 @@ import 'package:flutter/material.dart';
 import 'ui/app.dart';
 
 void main() async {
-  
-  runApp(const App());
-
+  WidgetsFlutterBinding.ensureInitialized();
   await CryptoService().initialize();
-  var encoded = await CryptoService().encode("1234567890");
-  print(encoded);
-  var decoded = await CryptoService().decode("tIHjeBm/5m+En42iVpEVFg==");
-  print(decoded);
-  var decoded2 = await CryptoService().decode("tIHjeBm/5m+En42iVpEVFg==");
-  print(decoded2);
-  var decoded3 = await CryptoService().decode("tIHjeBm/5m+En42iVpEVFg==");
-  print(decoded3);
+  runApp(const App());
 }
