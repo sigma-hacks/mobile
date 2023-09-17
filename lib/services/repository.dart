49bcbacef2ce1dd232e6d52fa08e9c041c4acef7
Artsiom Sazonau <artsiom.sazonau@kaseya.com>
2023-09-17 14:12:52 +0200

    add base class for cache and http requests

---
+++ lib/services/repository.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:ekzh/services/entities/registr.dart';
+import 'package:ekzh/services/https_service.dart';
+import 'package:ekzh/services/reachability_service.dart';
+
+class Repository {
+  final _httpService = HttpsService();
+  final _reachabilityServer = ReachabilityService();
+
+  late final Timer _timer;
+
+  Future updateRegister() async {
+    return _httpService.getRegistr(lastUpdate: DateTime.now());
+  }
+  
+  void startGettingRegistr() {
+    _timer = Timer.periodic(const Duration(seconds: 60), (timer) async { 
+      try {
+        final list = await _httpService.getRegistr(lastUpdate: DateTime.now());
+        // const entity = RegistrEntity(id: "123", name: "1234");
+        // update register
+      } catch (e) {
+        print(e.toString());
+      }
+    });
+  }
+
+  void stopGettingregistr() {
+    _timer.cancel();
+  }
+
+}
