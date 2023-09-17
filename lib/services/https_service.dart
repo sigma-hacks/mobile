18805d4d3ec80f9ad209a0666f16823bd53eeb9a
Artsiom Sazonau <artsiom.sazonau@kaseya.com>
2023-09-17 12:17:04 +0200

    add retry to auth

--- lib/services/https_service.dart
+++ lib/services/https_service.dart
@@ -1,11 +1,17 @@
 import 'dart:convert';
 import 'dart:async';
-import 'dart:isolate';
 import 'package:ekzh/services/entities/auth_entities.dart';
+import 'package:ekzh/services/reachability_service.dart';
 import 'package:ekzh/services/token_service.dart';
 import 'package:http/http.dart';
+import 'package:retry/retry.dart';
+
+enum AuthType {
+  password, pin
+}
 
 class HttpsService {
+  
   static final HttpsService _instance = HttpsService._internal();
 
   factory HttpsService() {
@@ -21,25 +27,39 @@ class HttpsService {
   final _client = Client();
   final _tokenService = TokenService();
 
-  Future<String> auth({required String email, required String pass}) async {
-    Map request = {"email": email, "password": pass};
-    Map<String, String> headers = {"Content-Type": "application/json"};
+  Future<String> auth({required String email, required String pass, required AuthType type}) async {
+    Map request = {
+      "email": email,
+      "password": pass,
+      "type": type == AuthType.password ? "password" : "pin",
+    };
+    Map<String, String>  headers = {
+      "Content-Type" : "application/json"
+    };
     final url = Uri.https(_baseUrl, '$_api/auth/login');
     final body = json.encode(request);
-    final response = await _client.post(
-      url,
-      headers: headers,
-      body: body,
+    return retry(
+      () async {
+        final isReachable = await ReachabilityService().isNetworkAvailable;
+        if (!isReachable) {
+          throw TimeoutException("custom");
+        }
+        final response = await _client.post( 
+          url, 
+          headers: headers, 
+          body: body,
+        );
+        if (response.statusCode == 200) {
+          var value = AuthResponse.fromJson(jsonDecode(response.body));
+          final token = value.data.token;
+          _tokenService.saveToken(token);
+          return token;
+        } else {
+          throw Exception("Failed to logIn");
+        }
+      },
+      maxAttempts: 3,
+      retryIf: (e) => e is TimeoutException,
     );
-    return await Isolate.run(() {
-      if (response.statusCode == 200) {
-        var value = AuthResponse.fromJson(jsonDecode(response.body));
-        final token = value.data.token;
-        _tokenService.saveToken(token);
-        return token;
-      } else {
-        throw Exception("Failed to logIn");
-      }
-    });
   }
-}
+  // MARK: - User  // Future<UserResponse> getUser() async {  //   final token = await TokenService().getToken();  //   if (token == null) {  //     throw Exception("There is no token in the system");  //   }   //   Map<String, String>  headers = {  //     "Content-Type" : "application/json",  //     "Authorization" : "Baerer $token"  //   };  //   final url = Uri.https(_baseUrl, '$_api/auth/register');  //   final response = await _client.get(  //     url,  //     headers: headers,  //   );  //   return await Isolate.run(() {  //     if (response.statusCode == 200) {  //       var value = RegisterResponse.fromJson(jsonDecode(response.body));  //       return value.data.token;  //     } else {  //       throw Exception("Failed to logIn");  //     }  //   });  //   return response;  // }}
