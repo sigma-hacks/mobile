3ea0049b8a4cb8dc5c25971ef43625ab447786e8
Artsiom Sazonau <artsiom.sazonau@kaseya.com>
2023-09-18 13:21:33 +0200

    fix main imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-import 'package:ekzh/services/Https_debug_support/my_http_overrides.dart';
+import 'package:ekzh/services/https_debug_support/my_http_overrides.dart';
 import 'package:ekzh/services/crypto_service.dart';
 import 'package:ekzh/services/https_service.dart';
 import 'package:flutter/material.dart';
 // import 'package:nfc_classic_mifare/nfc_classic_mifare.dart';
 
-import 'services/Https_debug_support/my_http_overrides.dart';
 import 'services/crypto_service.dart';
 import 'ui/app.dart';
 import 'dart:io';
