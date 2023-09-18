50da98495a4f0ad646471610912b07abcedcf75b
Mariya Pelageicheva <pelagei4eva@gmail.com>
2023-09-18 10:50:37 +0500

    fix

--- lib/services/repository.dart
+++ lib/services/repository.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:ekzh/services/entities/registr.dart';
+import 'package:ekzh/services/entities/register.dart';
 import 'package:ekzh/services/https_service.dart';
 import 'package:ekzh/services/reachability_service.dart';
 
@@ -13,9 +13,9 @@ class Repository {
   Future updateRegister() async {
     return _httpService.getRegistr(lastUpdate: DateTime.now());
   }
-  
+
   void startGettingRegistr() {
-    _timer = Timer.periodic(const Duration(seconds: 60), (timer) async { 
+    _timer = Timer.periodic(const Duration(seconds: 60), (timer) async {
       try {
         final list = await _httpService.getRegistr(lastUpdate: DateTime.now());
         // const entity = RegistrEntity(id: "123", name: "1234");
@@ -29,5 +29,4 @@ class Repository {
   void stopGettingregistr() {
     _timer.cancel();
   }
-
 }
