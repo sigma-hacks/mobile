0d5929adfd40b436869378709aa1224f530bb30b
Artsiom Sazonau <artsiom.sazonau@kaseya.com>
2023-09-19 00:42:19 +0200

    fix get registr metod

--- lib/services/entities/register_entities.dart
+++ lib/services/entities/register_entities.dart
@@ -2,34 +2,47 @@ import 'dart:core';
 
 import 'package:ekzh/services/entities/register.dart';
 import 'package:ekzh/services/entities/tariff.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'register_entities.g.dart';
+
+@JsonSerializable()
 class RegisterEntities {
-  final bool status;
+
+  @JsonKey(name: "time")
+  DateTime lastUpdated;
+  @JsonKey(name: 'tariffs')
   final List<Tariff> tariffs;
+  @JsonKey(name: 'names')
   final Map<String,dynamic> names;
+  @JsonKey(name: 'cards')
   final List<Register> registers;
 
   RegisterEntities({
-    required this.status,
+    required this.lastUpdated,
     required this.tariffs,
     required this.names,
     required this.registers,
   });
 
-  factory RegisterEntities.fromJson(Map<String, dynamic> json) {
-    final tariffs = json['data']['tariffs'] as List<dynamic>;
+  factory RegisterEntities.fromServer(Map<String, dynamic> json) {
+    final tariffs = json['tariffs'] as List<dynamic>;
     final finalTariffs = tariffs.map((e) => Tariff.fromJson(e)).toList();
 
-    final registers = json['data']['cards'] as List<dynamic>;
+    final registers = json['cards'] as List<dynamic>;
     final finalRegisters = registers.map((e) => Register.fromList(e)).toList();
 
-    final names = json['data']['names'] as Map<String,dynamic>;
+    final names = json['names'] as Map<String,dynamic>;
 
     return RegisterEntities(
-      status: json['status'] as bool,
+      lastUpdated: DateTime.now(),
       tariffs: finalTariffs,
       names: names,
       registers: finalRegisters,
     );
   }
+
+    factory RegisterEntities.fromJson(Map<String, dynamic> json) => _$RegisterEntitiesFromJson(json);
+
+    Map<String, dynamic> toJson() => _$RegisterEntitiesToJson(this);
 }
