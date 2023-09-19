fe1cc51268bece6d9e9f3c553beeb330ee13064a
Mariya Pelageicheva <pelagei4eva@gmail.com>
2023-09-19 23:19:45 +0500

    animation update cards

--- lib/ui/main_screen/widgets/registry_update.dart
+++ lib/ui/main_screen/widgets/registry_update.dart
@@ -1,8 +1,10 @@
 import 'package:ekzh/cubits/card_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 import '../../../common/theme/app_colors.dart';
+import '../../../models/state/card_state.dart';
 // import 'package:intl/intl.dart';
 
 class RegistryUpdate extends StatefulWidget {
@@ -17,53 +19,96 @@ class RegistryUpdate extends StatefulWidget {
 }
 
 class _RegistryUpdateState extends State<RegistryUpdate> {
-
   DateTime? lastUpdate;
+  bool isLoading = false;
 
   Future<void> getCards() async {
+    setState(() {
+      isLoading = true;
+    });
     final postsCubit = context.read<CardCubit>();
     await postsCubit.getCards();
+    await lastTime();
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<void> lastTime() async {
+    final postsCubit = context.read<CardCubit>();
+    lastUpdate = await postsCubit.getLastUpdateDateTime();
+    setState(() {});
   }
 
   @override
   void initState() {
     super.initState();
-    final postsCubit = context.read<CardCubit>();
-    postsCubit.getLastUpdateDateTime().then((value) {
-      setState(() {
-        this.lastUpdate = value;
-      });
-    });
+    lastTime();
   }
 
   @override
   Widget build(BuildContext context) {
+    return isLoading
+        ? _RegistryUpdating()
+        : InkWell(
+            onTap: () async {
+              await getCards();
+            },
+            child: Ink(
+                child: Row(
+              children: [
+                Icon(
+                  widget.isConnect ? Icons.update : Icons.update_disabled,
+                  color: AppColors.grey,
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(widget.isConnect
+                          ? 'Обновить реестр'
+                          : 'Невозможно обновить реестр'),
+                      Text(
+                        lastUpdate != null
+                            ? 'последнее обновление ${DateFormat('dd.MM.yyyy').format(lastUpdate!)} в ${DateFormat('HH:mm').format(lastUpdate!)}'
+                            : 'Обновлений не было',
+                        style: Theme.of(context).textTheme.bodySmall,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )),
+          );
+  }
+}
 
-    return InkWell(
-      onTap: () async {
-        await getCards();
-      },
-      child: Ink(
-          child: Row(
-        children: [
-          Icon(
-            widget.isConnect ? Icons.update : Icons.update_disabled,
-            color: AppColors.grey,
+class _RegistryUpdating extends StatelessWidget {
+  const _RegistryUpdating({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const SizedBox.square(
+          dimension: 24,
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(
+              AppColors.grey,
+            ),
+            strokeWidth: 2,
           ),
-          const SizedBox(width: 8),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                  widget.isConnect ? 'Обновить реестр' : 'Невозможно обновить реестр'),
-              Text(
-                lastUpdate != null ? 'последнее обновление ${lastUpdate!.day}.${lastUpdate!.month}.${lastUpdate!.year} в ${lastUpdate!.hour}:${lastUpdate!.minute}' : 'Обновлений не было',
-                style: Theme.of(context).textTheme.bodySmall,
-              ),
-            ],
-          ),
-        ],
-      )),
+        ),
+        const SizedBox(width: 8),
+        Text(
+          'Реестр обновляется',
+          style: Theme.of(context)
+              .textTheme
+              .bodyMedium!
+              .copyWith(color: AppColors.grey),
+        ),
+      ],
     );
   }
 }
