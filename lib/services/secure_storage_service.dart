dcd4562f875405e170cab2b523a9ebffdd019f2c
Artsiom Sazonau <artsiom.sazonau@kaseya.com>
2023-09-19 19:41:23 +0200

    Remove comments

--- lib/services/secure_storage_service.dart
+++ lib/services/secure_storage_service.dart
@@ -11,21 +11,6 @@ class SecureStorageService {
 // Create storage
   final FlutterSecureStorage storage = FlutterSecureStorage(aOptions: AndroidOptions(encryptedSharedPreferences: true));
 
-  
-// // Read value
-// String value = await storage.read(key: key);
-
-// // Read all values
-// Map<String, String> allValues = await storage.readAll();
-// // Delete value
-// await storage.delete(key: key);
-
-// // Delete all
-// await storage.deleteAll();
-
-// // Write value
-// await storage.write(key: key, value: value);
-
   factory SecureStorageService() {
     return _instance;
   }
