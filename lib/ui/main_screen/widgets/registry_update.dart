ac40e4eb4e2197aa5406e6d37ee267f050e4504f
Artsiom Sazonau <artsiom.sazonau@kaseya.com>
2023-09-19 17:42:32 +0200

    add action, change to Stateful

--- lib/ui/main_screen/widgets/registry_update.dart
+++ lib/ui/main_screen/widgets/registry_update.dart
@@ -1,8 +1,10 @@
+import 'package:ekzh/services/card_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../common/theme/app_colors.dart';
 
-class RegistryUpdate extends StatelessWidget {
+class RegistryUpdate extends StatefulWidget {
   final bool isConnect;
   const RegistryUpdate({
     super.key,
@@ -10,14 +12,27 @@ class RegistryUpdate extends StatelessWidget {
   });
 
   @override
+  State<RegistryUpdate> createState() => _RegistryUpdateState();
+}
+
+class _RegistryUpdateState extends State<RegistryUpdate> {
+
+  Future<void> getCards() async {
+    final postsCubit = context.read<CardCubit>();
+    await postsCubit.getCards();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () async {
+        await getCards();
+      },
       child: Ink(
           child: Row(
         children: [
           Icon(
-            isConnect ? Icons.update : Icons.update_disabled,
+            widget.isConnect ? Icons.update : Icons.update_disabled,
             color: AppColors.grey,
           ),
           const SizedBox(width: 8),
@@ -25,7 +40,7 @@ class RegistryUpdate extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                  isConnect ? 'Обновить реестр' : 'Невозможно обновить реестр'),
+                  widget.isConnect ? 'Обновить реестр' : 'Невозможно обновить реестр'),
               Text(
                 'последнее обновление в 14:09',
                 style: Theme.of(context).textTheme.bodySmall,
